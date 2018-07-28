e1e98e087ba480480e5fb30fbc66c245181d1ff7
Oleg Jakunin <oleg.kemz@gmail.com>
2018-07-28 15:32:19 +0500

    shared examples via advanced rspec

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -2,17 +2,7 @@ require 'rails_helper'
 
 describe 'questions API' do
   describe 'GET #index' do
-    context 'unauthorized' do
-      it 'returns 401 status if there is no access_token' do
-        get '/api/v1/questions/', params: { format: :json }
-        expect(response.status).to eq 401
-      end
-
-      it 'returns 401 status if access_token is invalid' do
-        get '/api/v1/questions/', params: { format: :json, access_token: '1234' }
-        expect(response.status).to eq 401
-      end
-    end
+    it_behaves_like "API Authenticable"
 
     context 'authorized' do
       let(:user) { create(:user) }
@@ -25,13 +15,9 @@ describe 'questions API' do
         params: { format: :json, access_token: access_token.token }
       end
 
-      it 'returns 200 status' do
-        expect(response).to be_successful
-      end
+      it_behaves_like "response successful"
 
-      it 'returns list of questions' do
-        expect(response.body).to have_json_size(3)
-      end
+      it_behaves_like "return array size", 'questions', 3
 
       %w(id title body created_at updated_at).each do |attr|
         it "question object contains #{attr}" do
@@ -41,22 +27,18 @@ describe 'questions API' do
         end
       end
     end
+
+    def do_request(options = {})
+      get '/api/v1/questions/', params: { format: :json }.merge(options)
+    end
   end
 
   describe 'GET #show' do
-    context 'unauthorized' do
+    context 'unathorized' do
       let(:user) { create(:user) }
       let(:question) { create :question, user: user }
 
-      it 'returns 401 status if there is no access_token' do
-        get "/api/v1/questions/#{question.id}", params: { format: :json }
-        expect(response.status).to eq 401
-      end
-
-      it 'returns 401 status if access_token is invalid' do
-        get "/api/v1/questions/#{question.id}", params: { format: :json, access_token: '1234' }
-        expect(response.status).to eq 401
-      end
+      it_behaves_like "API Authenticable"
     end
 
     context 'authorized' do
@@ -74,17 +56,11 @@ describe 'questions API' do
         params: { format: :json, access_token: access_token.token }
       end
 
-      it 'returns 200 status' do
-        expect(response).to be_successful
-      end
+      it_behaves_like "response successful"
 
-      it 'returns comments for question' do
-        expect(response.body).to have_json_size(3).at_path('comments')
-      end
+      it_behaves_like "return array size", 'comments', 3, 'comments'
 
-      it 'returns attachments for question' do
-        expect(response.body).to have_json_size(3).at_path('attachments')
-      end
+      it_behaves_like "return array size", 'attachments', 3, 'attachments'
 
       it "attachments object contains file" do
         expect(response.body).to be_json_eql(attachment.file.to_json).at_path('attachments/0/file')
@@ -105,20 +81,13 @@ describe 'questions API' do
       end
     end
 
+    def do_request(options = {})
+     get "/api/v1/questions/#{question.id}", params: { format: :json }.merge(options)
+    end
   end
 
   describe 'POST #create' do
-    context 'unauthorized' do
-      it 'returns 401 status if there is no access_token' do
-        post "/api/v1/questions", params: { format: :json }
-        expect(response.status).to eq 401
-      end
-
-      it 'returns 401 status if access_token is invalid' do
-        post "/api/v1/questions", params: { format: :json, access_token: '1234' }
-        expect(response.status).to eq 401
-      end
-    end
+     it_behaves_like "API Authenticable"
 
     context 'authorized' do
       let(:user) { create(:user) }
@@ -152,5 +121,9 @@ describe 'questions API' do
                                           access_token: access_token.token
                                         }
     end
+  end
+
+  def do_request(options = {})
+    post "/api/v1/questions/", params: { format: :json }.merge(options)
   end
 end
